b37adb3c1e06524255588dcd5e2f69d4f71e2bfe
Rueber <rueb8071@vandals.uidaho.edu>
2023-02-01 13:53:00 -0800

    Lots of work - Billy Gnosis

--- header.js
+++ header.js
@@ -2,12 +2,12 @@
 
 let appHeader = `
 <nav id="header-nav">
-	<a class="header-nav-item" href="./index.html">Home</a>
-	<a class="header-nav-item" href="./about.html">About</a>
-	<a class="header-nav-item" href="./portfolio.html">Portfolio</a>
-	<a class="header-nav-item" href="./contact.html">Contact</a>
-	<a class="header-nav-item" href="./signin.html">Sign in</a>
-	<a class="header-nav-item" href="./signup.html">Sign up</a>
+	<a class="header-nav-item" id="home-header-link" href="./index.html">Home</a>
+	<a class="header-nav-item" id="about-header-link" href="./about.html">About</a>
+	<a class="header-nav-item" id="portfolio-header-link" href="./portfolio.html">Portfolio</a>
+	<a class="header-nav-item" id="contact-header-link" href="./contact.html">Contact</a>
+	<a class="header-nav-item" id="signin-header-link" href="./signin.html">Sign in</a>
+	<a class="header-nav-item" id="signup-header-link" href="./signup.html">Sign up</a>
 </nav>
 `;
 document.getElementById("header").innerHTML = appHeader;
