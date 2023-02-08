50119c1b9bb37227c327935d200f82f4b9afc570
CarsonRueber <carsonrueber@gmail.com>
2023-02-08 11:01:10 -0800

    Finish the thing

--- footer.js
+++ footer.js
@@ -1,6 +1,10 @@
 //
 
 let appFooter = `
-
+<nav id="footer-nav">
+Cookie Policy: N/A<br>
+Terms and Conditions: N/A<br>
+Privacy Policy: N/A
+</nav>
 `;
 document.getElementById("footer").innerHTML = appFooter;
